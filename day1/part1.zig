f22be7d2828b38df830f77d6b98b34e19a010ca3
Hong Shick Pak <hong@hspak.com>
2019-12-01 14:19:52 -0800

    Minimize Day 1 loc for kicks

--- day1/part1.zig
+++ day1/part1.zig
@@ -1,16 +1,7 @@
 const std = @import("std");
-const fs = std.fs;
-const io = std.io;
-const warn = std.debug.warn;
-
-var allocator: *std.mem.Allocator = undefined;
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
-    defer arena.deinit();
-    allocator = &arena.allocator;
-
-    var file = try fs.File.openRead("./input");
+    var file = try std.fs.File.openRead("./input");
     defer file.close();
     var stream = &file.inStream().stream;
 
@@ -18,7 +9,7 @@ pub fn main() !void {
     var total_fuel: u32 = 0;
     while (try stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         const mass = try std.fmt.parseInt(u32, line[0..], 10);
-        total_fuel += @divFloor(mass, 3) - 2;
+        total_fuel += mass / 3 - 2;
     }
-    warn("{}\n", total_fuel);
+    std.debug.warn("{}\n", total_fuel);
 }
